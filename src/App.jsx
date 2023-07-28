d32c3ae2205eee47bab300014de2715130c52930
jahidhasan6484 <jahidhasan6484@gmail.com>
2023-07-28 16:14:43 +0600

    responsive done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import socialMediaData from "./Data/socialMedia";
 import SocialCard from "./ReusableComponents/SocialCard";
@@ -11,14 +10,16 @@ function App() {
   }, []);
 
   return (
-    <div className="h-screen">
-      <div className="bg-primary flex items-center justify-center h-1/4">
-        <h1 className="text-white text-xl">How did you hear about us?</h1>
+    <div className="h-auto xl:h-screen">
+      <div className="bg-primary flex items-center justify-center h-32 xl:h-1/4">
+        <h1 className="text-white text-xl xl:text-3xl font-normal ">
+          How did you hear about us?
+        </h1>
       </div>
 
-      <div className="bg-lightGray h-3/4 pt-4">
-        <div className="w-1/3 mx-auto space-y-12">
-          <div className="grid grid-cols-3 gap-8">
+      <div className="bg-lightGray h-3/4 pt-8">
+        <div className="w-full px-4 xl:w-1/3 mx-auto space-y-12">
+          <div className="grid grid-cols-2 xl:grid-cols-3 gap-8">
             {socialMediaList.length > 0 &&
               socialMediaList.map((media, index) => {
                 const { name, imageURL } = media;
--- src/ReusableComponents/SocialCard.jsx
+++ src/ReusableComponents/SocialCard.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 const SocialCard = ({ name, imageURL }) => {
   return (
-    <div className="bg-white flex flex-col items-center gap-4 py-4 rounded-lg hover:bg-primary text-black hover:text-white cursor-pointer">
+    <div className="bg-white flex flex-col items-center gap-2 md:gap-4 py-4 rounded-lg hover:bg-primary text-black hover:text-white cursor-pointer">
       <img
-        className="border-2 border-gray rounded-full p-4 w-16 bg-white"
+        className="border md:border-2 border-gray rounded-full p-4 w-16 bg-white"
         src={imageURL}
         alt={name}
       />
